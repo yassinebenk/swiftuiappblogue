0073ec20f204edd1ddf3f66c9fd09ae6174425eb
Yassine Ben-khayat <2175280@crosemont.qc.ca>
2025-02-15 01:59:44 -0500

    ajout projet swiftui

---
+++ BlogueSwiftUI/BlogueSwiftUI/BlogueSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlogueSwiftUIApp.swift
+//  BlogueSwiftUI
+//
+//  Created by Yassine Ben-khayat on 2025-02-15.
+//
+
+import SwiftUI
+
+@main
+struct BlogueSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlogueSwiftUI/BlogueSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BlogueSwiftUI
+//
+//  Created by Yassine Ben-khayat on 2025-02-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
