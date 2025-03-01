9d75b5350985a323e8082c581bdfb2f0da75897f
Yassine Ben-khayat <2175280@crosemont.qc.ca>
2025-03-01 03:30:03 -0500

    demo 2 navigationStack

--- BlogueSwiftUI/BlogueSwiftUI/ChatonVue.swift
+++ BlogueSwiftUI/BlogueSwiftUI/ChatonVue.swift
@@ -8,70 +8,48 @@
 import SwiftUI
 
 struct ChatonVue: View {
+    var animaux : [Animal] = [.init(nom: "Chaton", couleur: .blue, nomImage: "kitten2"), .init(nom: "Chien", couleur: .yellow, nomImage: "chien3"), .init(nom: "Hamster", couleur: .gray, nomImage: "hamster2")]
+    
+    
     var body: some View {
-        NavigationView{
-            VStack{
-                VueCercleTemplate(couleur: Color(hue: 0.516, saturation: 1.0, brightness: 1.0), nomImage: "kitten2")
-                    .navigationTitle("Chaton")
-                    .offset(y: -60)
-                
-                NavigationLink(destination: ChienVue(), label: {
-                    Text("Chien")
-                        .bold().frame(width: 280, height: 50)
-                        .background(Color .blue)
-                        .foregroundColor(.white).cornerRadius(10)
-                })
-            }
+        NavigationStack{
+            List {
+                Section("Liste d'animaux") {
+                    ForEach(animaux, id: \.nom) {animal in
+                        NavigationLink(value: animal){
+                            HStack{
+                                Image(animal.nomImage).resizable().scaledToFit().frame(width: 50, height: 50).clipShape(.circle).overlay{
+                                    Circle().stroke(animal.couleur, lineWidth : 2)
+                                }
+                                Text(animal.nom)
+                                    .offset(x: 5).foregroundColor(animal.couleur)
+                            }
+                        }
+                    }
+                }
+            }.navigationTitle("Mes animaux")
+                .navigationDestination(for: Animal.self) { animal in
+                    ZStack{
+                        animal.couleur.ignoresSafeArea()
+                        HStack{
+                            Image(animal.nomImage).resizable().scaledToFit().frame(width: 100, height: 100).clipShape(.circle).overlay{
+                                Circle().stroke(.black, lineWidth : 2)
+                            }
+                            Text(animal.nom)
+                                .offset(x: 5).foregroundColor(.black).font(.largeTitle).bold()
+                        }
+                    }
+                    
+                }
         }.accentColor(Color(.label))
     }
 }
-
-struct ChienVue: View {
-    var body: some View {
-        VStack{
-            VueCercleTemplate(couleur: Color .yellow, nomImage: "chien3")
-                .navigationTitle("Chien")
-                .offset(y: -60)
-            
-            NavigationLink(destination: HamsterVue(), label: {
-                Text("Hamster")
-                    .bold().frame(width: 280, height: 50)
-                    .background(Color .orange)
-                    .foregroundColor(.white).cornerRadius(10)
-            })
-        }
-    }
-}
-
-
-struct HamsterVue: View {
-    var body: some View {
-        VStack{
-            VueCercleTemplate(couleur: Color .gray, nomImage: "hamster2")
-                .navigationTitle("Hamster")
-                .offset(y: -60)
-        }
-    }
-}
-
-
-
-struct VueCercleTemplate: View {
-    var couleur : Color
-    var nomImage : String
     
-    var body: some View{
-        ZStack {
-            Circle()
-                .frame(width: 300, height: 300)
-                .foregroundColor(couleur)
-            Image(nomImage)
-                .resizable()
-                .scaledToFit()
-                .frame(width: 250, height: 250)
-                .clipShape(.circle)
-        }
-    }
+    
+struct Animal : Hashable{
+    let nom: String
+    let couleur : Color
+    let nomImage : String
 }
 
 #Preview {
