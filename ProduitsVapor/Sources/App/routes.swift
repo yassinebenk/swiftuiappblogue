92da094e981a27856f242ce7c34051b8794ee5e9
Yassine Ben-khayat <2175280@crosemont.qc.ca>
2025-03-15 03:58:19 -0400

    ajout img et nv code

--- ProduitsVapor/Sources/App/routes.swift
+++ ProduitsVapor/Sources/App/routes.swift
@@ -9,6 +9,13 @@ func routes(_ app: Application) throws {
     app.get("hello") { req async -> String in
         "Hello, world!"
     }
+    
+    app.get("hello", ":nom") { req async throws -> String in
+        
+        let nom = try req.parameters.require("nom")
+        
+        return "Hello, \(nom.capitalized)!"
+    }
 
     try app.register(collection: TodoController())
 }
