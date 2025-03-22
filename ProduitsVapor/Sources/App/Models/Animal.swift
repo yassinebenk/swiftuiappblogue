5a7c6b320c80320a78aad08925b2e55f97bd3659
Yassine Ben-khayat <2175280@crosemont.qc.ca>
2025-03-22 12:00:09 -0400

    blogue 7 api

---
+++ ProduitsVapor/Sources/App/Models/Animal.swift
@@ -0,0 +1,14 @@
+//
+//  Animal.swift
+//  ProduitsVapor
+//
+//  Created by Yassine Ben-khayat on 2025-03-22.
+//
+import Vapor
+
+struct Animal: Content {
+    let id: Int
+    let nom: String
+    let couleur: String
+    let nomImage: String
+}
--- ProduitsVapor/Sources/App/routes.swift
+++ ProduitsVapor/Sources/App/routes.swift
@@ -16,6 +16,33 @@ func routes(_ app: Application) throws {
         
         return "Hello, \(nom.capitalized)!"
     }
+    
+    app.get("animaux") { req async -> [Animal] in
+        return [
+            Animal(id: 1, nom: "Chaton", couleur: "blue", nomImage: "image1"),
+            Animal(id: 2, nom: "Chien", couleur: "yellow", nomImage: "image2"),
+            Animal(id: 3, nom: "Hamster", couleur: "gray", nomImage: "image3")
+        ]
+    }
+    
+    app.get("animaux", ":id") { req async throws -> Animal in
+        guard let idString = req.parameters.get("id"),
+              let id = Int(idString) else {
+            throw Abort(.badRequest, reason: "id invalide")
+        }
+        
+        let animals = [
+            Animal(id: 1, nom: "Chaton", couleur: "blue", nomImage: "image1"),
+            Animal(id: 2, nom: "Chien", couleur: "yellow", nomImage: "image2"),
+            Animal(id: 3, nom: "Hamster", couleur: "gray", nomImage: "image3")
+        ]
+        
+        if let animal = animals.first(where: { $0.id == id }) {
+            return animal
+        } else {
+            throw Abort(.notFound, reason: "Animal avec l'id \(id) non existant")
+        }
+    }
 
     try app.register(collection: TodoController())
 }
